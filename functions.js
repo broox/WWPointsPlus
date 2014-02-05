370bf8cf1e8953f5bebb6a91031d69d2e7b57b99
KevinSwitzer <kevineswitzer@gmail.com>
2014-02-05 17:12:29 -0500

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,10 @@
+<!-- 
+
+function getPoints(){
+	$.post("functions.php", { function:"getPoints",calories:document.getPoints.calories.value,fat:document.getPoints.fat.value,fiber:document.getPoints.fiber.value },
+	function(data){
+		$('#result').html(data);
+	});
+}
+
+-->
